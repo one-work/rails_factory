332cc8ad56cf676690ed1b88fed4a3cdd5d50297
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-04 21:26:47 +0800

    barcode

--- app/models/factory/model/barcode.rb
+++ app/models/factory/model/barcode.rb
@@ -14,20 +14,20 @@ module Factory
 
       index :gtin, unique: true
 
-      belongs_to :unit, class_name: 'Trade::Unit'
+      belongs_to :unit, class_name: 'Trade::Unit', optional: true
 
-      belongs_to :brand
+      belongs_to :brand, optional: true
 
       before_validation :init_brand
       before_validation :init_unit
     end
 
     def init_brand
-      self.brand ||= Brand.find_or_create_by(name: brand_name)
+      self.brand ||= Brand.find_or_create_by(name: brand_name) if brand_name.present?
     end
 
     def init_unit
-      self.unit ||= Trade::Unit.find_or_create_by(name: unit_name)
+      self.unit ||= Trade::Unit.find_or_create_by(name: unit_name) if unit_name.present?
     end
 
     class_methods do
@@ -35,7 +35,7 @@ module Factory
       def init_from_csv(path = Rails.root.join('storage/barcodes.csv'))
         CSV.foreach(path, headers: true) do |row|
           begin
-            self.create(row.to_hash.slice('gtin', 'name', 'spec', 'brand_name', 'unit_name', 'supplier', 'made_in', 'price'))
+            self.create!(row.to_hash.slice('gtin', 'name', 'spec', 'brand_name', 'unit_name', 'supplier', 'made_in', 'price'))
           rescue => e
             puts e.message
           end
