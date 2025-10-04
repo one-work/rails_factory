59b07d393e0195ae226d6fa1217adaec8d774f64
qinmingyuan <mingyuan0715@foxmail.com>
2025-10-04 15:13:34 +0800

    moved qingflow

--- app/models/factory/product.rb
+++ app/models/factory/product.rb
@@ -2,6 +2,5 @@ module Factory
   class Product < ApplicationRecord
     include Model::Product
     include Auditor::Ext::Discard if defined? RailsAudit
-    include Qingflow::Ext::Related if defined? RailsQingflow
   end
 end
