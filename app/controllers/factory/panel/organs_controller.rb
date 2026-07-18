173c1c31cab50ab25c318a9fb89cc4104a7c2e8a
qinmingyuan <mingyuan0715@foxmail.com>
2026-07-18 13:52:29 +0800

    order desc

--- app/controllers/factory/panel/organs_controller.rb
+++ app/controllers/factory/panel/organs_controller.rb
@@ -2,7 +2,7 @@ module Factory
   class Panel::OrgansController < Org::Panel::OrgansController
 
     def index
-      @organs = Organ.includes(:provider).roots.page(params[:page])
+      @organs = Organ.includes(:provider).roots.order(id: :desc).page(params[:page])
     end
 
     private
