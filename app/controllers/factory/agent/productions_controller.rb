d8dbb43fd3a8135a4b06af82c731354f717b6a1b
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-21 15:23:59 +0800

    topbar

--- app/controllers/factory/agent/productions_controller.rb
+++ app/controllers/factory/agent/productions_controller.rb
@@ -6,6 +6,7 @@ module Factory
     before_action :set_card_templates, only: [:index]
     before_action :set_scene, only: [:index], if: -> { params[:scene_id].present? }
     before_action :set_desk, only: [:index], if: -> { params[:desk_id].present? }
+    before_action :set_contact, only: [:index], if: -> { params[:contact_id].present? }
     before_action :set_cart, only: [:index, :nav, :show, :dialog, :create_dialog]
 
     def index
@@ -56,6 +57,10 @@ module Factory
 
     def set_scene
       @scene = Scene.find params[:scene_id]
+    end
+
+    def set_contact
+      @contact = Contact.find params[:contact_id]
     end
 
     def set_desk
