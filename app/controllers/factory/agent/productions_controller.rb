aec39a0d31308457b4fbb768c7f0fa805a5ddc7a
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-23 12:54:46 +0800

    fixed

--- app/controllers/factory/agent/productions_controller.rb
+++ app/controllers/factory/agent/productions_controller.rb
@@ -7,7 +7,7 @@ module Factory
     before_action :set_scene, only: [:index], if: -> { params[:scene_id].present? }
     before_action :set_desk, only: [:index], if: -> { params[:desk_id].present? }
     before_action :set_contact, only: [:index], if: -> { params[:contact_id].present? }
-    before_action :set_client, only: [index], if: -> { params[:client_id].present? }
+    before_action :set_client, only: [:index], if: -> { params[:client_id].present? }
     before_action :set_cart, only: [:index, :nav, :show, :dialog, :create_dialog]
 
     def index
